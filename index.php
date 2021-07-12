02f073faf098b7bedc36b4adf7bd6de269bf5cfe
ali shabazpor <77952910+isoplus1@users.noreply.github.com>
2021-07-12 17:22:32 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="files/css/stylle.css"/>
+        <title>hello html</title>
+
+</head>
+    <body>
+       <section class="background">
+<form action="pay.php" method="post">
+    <input type="text" name="name" placeholder=" Name" required>
+    <input type="text" name="Amount" placeholder=" Amount" required>
+    <input type="text" name="Description" placeholder=" Description"><br>
+    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" required> 
+    <label for="vehicle1"> I agree with the rules</label><br>
+
+    <input type="submit" value="buy">
+</form>
+       </section>
+    </body>
+</html>
